d49a195f8b49cb94dc9efa49e1e829cde48c7b52
jonaiker-dev <riordan1783@gmail.com>
2025-04-05 00:50:20 -0500

    probando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FormularioVentas';
+  public FormVentas:FormGroup;
+  private Webhook_URL="https://script.google.com/macros/s/AKfycbzMXhfWtfI1ze1Bl09zsP4Ey22raQoOiaV7MwxGJB8coTsHab58WZNfAiaOev3Vlu6ghg/exec"
+  submissionStatus:String=''
+  constructor(private fb:FormBuilder,private http:HttpClient){
+    this.FormVentas=this.fb.group({
+      categoria: ['', Validators.required],
+      nombre: ['', Validators.required],
+      cantidad: ['', Validators.required],
+      precio: ['', Validators.required],
+      metodo: ['', Validators.required],
+      vendedor: ['', Validators.required]
+    })
+
+  }
+
+  Enviar(){
+    
+    if (this.FormVentas.valid) {
+      this.submissionStatus = 'Enviando datos...';
+      this.http.post(this.Webhook_URL, this.FormVentas.value)
+        .subscribe(
+          (response: any) => {
+            this.submissionStatus = 'Datos enviados correctamente';
+            console.log('Respuesta del webhook:', response);
+            this.FormVentas.reset(); // Opcional: Limpiar el formulario
+          },
+          (error) => {
+            this.submissionStatus = 'Error al enviar los datos';
+            console.error('Error del webhook:', error);
+          }
+        );
+    } else {
+      this.submissionStatus = 'Por favor, completa todos los campos correctamente.';
+    }
+    console.log(this.submissionStatus);
+  }
+  
+
 }
