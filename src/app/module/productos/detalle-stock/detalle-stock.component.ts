5356c8f366a275a8d5b3476a53408afacc2551f0
jonaiker-dev <riordan1783@gmail.com>
2025-04-11 18:20:39 -0500

    sort desc product history

--- src/app/module/productos/detalle-stock/detalle-stock.component.ts
+++ src/app/module/productos/detalle-stock/detalle-stock.component.ts
@@ -17,7 +17,19 @@ export class DetalleStockComponent implements OnInit {
       this.route.paramMap.subscribe(params=>{
         this.id=params.get('id')
         this.ps.GetProductHistory(this.id).subscribe((data:any)=>{
-          this.datahistorial=data
+          this.datahistorial=data.sort((a:any,b:any)=>{
+            if (b.id > a.id) {
+              return 1;
+            }
+           
+            if (a.id > b.id) {
+              return -1;
+            }
+            
+            return 0
+          })
+          console.log(this.datahistorial);
+        
         })
       })
     }
